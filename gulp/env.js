51f9c77c8e7fc31e4348479b0789c5671b32cf62
Filip Rastovic <filiporastovic@gmail.com>
2019-06-01 14:31:58 +0200

    -initial commit

---
+++ gulp/env.js
@@ -0,0 +1,10 @@
+module.exports = {
+    // Settings
+    liveReload: true,
+    // BrowserSync Configuration
+    siteUrl: 'localhost:3000',
+    port: 3000,
+    // BrowserSync File Path Configuration
+    templatePath: '../index.html',
+    assetsPath: '../assets/**/**'
+};
